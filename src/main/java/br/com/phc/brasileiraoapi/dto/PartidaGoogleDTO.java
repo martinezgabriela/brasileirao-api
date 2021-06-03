7727d47fb2a08d8d9e4e10fefc466e699cd44269
Gabriela Martinez <gabrielamartinez.19@gmail.com>
2021-06-03 15:07:02 -0300

    primeira conexão com o jsoup no google

--- src/main/java/br/com/phc/brasileiraoapi/dto/PartidaGoogleDTO.java
+++ src/main/java/br/com/phc/brasileiraoapi/dto/PartidaGoogleDTO.java
@@ -2,6 +2,15 @@ package br.com.phc.brasileiraoapi.dto;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PartidaGoogleDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
